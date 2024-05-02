fe376ddeb8f0d85beb9b52610170153bc8b72b1d
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-02 15:48:30 +0200

    added structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+include __DIR__ ."/Models/hotels.php";
+var_dump($hotels);
+
+include __DIR__ ."/Views/header.php";
+?>
+<main>
+    <div class="container">
+        <?php
+    ?>  
+
+    </div>
+</main>
+</body>
+</html>
